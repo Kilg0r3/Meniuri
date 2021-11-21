3e4d30304d41725e66660ac74b7cbcc394134f4c
Kilg0r3 <negulescuandrei97@gmail.com>
2021-11-21 21:38:31 +0200

    Fixed menu double content display.

--- src/org/andrei/menus/MainMenu.java
+++ src/org/andrei/menus/MainMenu.java
@@ -1,15 +1,10 @@
 package org.andrei.menus;
 
 public class MainMenu extends Menu {
-	private StringBuilder menuBuilder;
-
-	public MainMenu() {
-		menuBuilder = new StringBuilder();
-	}
 
 	public void displayMenu() {
-		menuBuilder.append("Main Menu\n")
-				.append("=========\n")
+		StringBuilder menuBuilder = new StringBuilder("Main Menu\n");
+		menuBuilder.append("=========\n")
 				.append("1. Menu One\n")
 				.append("2. Menu two\n");
 		System.out.println(menuBuilder);
